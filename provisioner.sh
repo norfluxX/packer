75b5075c0d1f5e21947096e5ad1f0c272a766b02
Bhikesh Khute <35907619+norfluxX@users.noreply.github.com>
2024-05-07 22:03:42 +0530

    Update provisioner.sh

--- provisioner.sh
+++ provisioner.sh
@@ -1,24 +1,10 @@
 #!/usr/bin/env bash
 
 # update package
-sudo yum -y update
+sudo apt-get update
 
-# install git
-sudo yum install git -y
+#install neofetch
+sudo apt-get install neofetch -y
 
-# install SSM
-sudo yum install https://s3.amazonaws.com/ec2-downloads-windows/SSMAgent/latest/linux_amd64/amazon-ssm-agent.rpm -y
-#sudo systemctl start amazon-ssm-agent
-
-# install cloudwatch agent
-sudo yum install https://s3.amazonaws.com/amazoncloudwatch-agent/amazon_linux/amd64/latest/amazon-cloudwatch-agent.rpm -y
-sudo /opt/aws/amazon-cloudwatch-agent/bin/amazon-cloudwatch-agent-ctl -m ec2 -a start
-sudo /opt/aws/amazon-cloudwatch-agent/bin/amazon-cloudwatch-agent-ctl -m ec2 -a status
-
-# install AWS inspector
-curl -O https://inspector-agent.amazonaws.com/linux/latest/install
-sudo bash install
-
-#insall docker 
-sudo yum install docker -y
-sudo systemctl start docker
+#LoginPermanent
+sudo bash -c $'echo "neofetch" >> /etc/profile.d/mymotd.sh && chmod +x /etc/profile.d/mymotd.sh'
